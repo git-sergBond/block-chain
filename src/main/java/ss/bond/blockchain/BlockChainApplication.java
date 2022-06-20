1c7e6e6810169a6d01094d0924a6345b8250b4d4
git-sergBond <git-serg.bond@yandex.ru>
2022-06-20 12:13:13 +0300

    Initial commit

---
+++ src/main/java/ss/bond/blockchain/BlockChainApplication.java
@@ -0,0 +1,13 @@
+package ss.bond.blockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockChainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ss/bond/blockchain/BlockChainApplicationTests.java
@@ -0,0 +1,13 @@
+package ss.bond.blockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
