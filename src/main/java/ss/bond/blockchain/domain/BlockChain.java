17abeac0e6964d97786decb48fd883606010daef
git-sergBond <git-serg.bond@yandex.ru>
2022-06-20 14:04:17 +0300

    add func hash

--- src/main/java/ss/bond/blockchain/domain/BlockChain.java
+++ src/main/java/ss/bond/blockchain/domain/BlockChain.java
@@ -1,8 +1,14 @@
 package ss.bond.blockchain.domain;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.SortedMap;
 
 public class BlockChain {
 
@@ -20,7 +26,7 @@ public class BlockChain {
      * @return новый блок
      */
     public Map<String, String> newBlock(String previousHash) {
-        Map<String, String> block = new java.util.HashMap<>(Map.of(
+        TreeMap<String, String> block = new java.util.TreeMap<>(Map.of(
                 "index", String.valueOf(chain.size()),
                 "timestamp", LocalDateTime.now().toString(), //TODO посмотреть чтобы была timeZone и формат даты ISO
                 "previousHash", previousHash
@@ -37,10 +43,20 @@ public class BlockChain {
      * Хэширует блок.
      *
      * @param block Блок от которого нужно получить Хэш
-     * @return Хэш в формате SHA256
+     * @return Хэш в формате SHA256 закодированный в BASE64
      */
-    public static String hash(Map<String, String> block) {
-        return null;
+    public static String hash(SortedMap<String, String> block) {
+        String unifiedBlock = getUnifiedBlock(block);
+        byte[] rawBlock = unifiedBlock.getBytes(StandardCharsets.UTF_8);
+        byte[] hash = null;
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            hash = messageDigest.digest(rawBlock);//TODO проверить нужен ли цикличный вызов update (вдруг есь ограничение по длинне массива байт)
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        String hashBase64 = Base64.getEncoder().encodeToString(hash);//TODO проверить нужно ли тут явно указвывать StandardCharsets.UTF_8 . PS хотя и так понятно, что BASE64 использует какаую-то универсальную кодировку
+        return hashBase64;
     }
 
     /**
@@ -71,5 +87,28 @@ public class BlockChain {
      */
     private void createGenesisBlock() {
         newBlock(null);
+    }
+
+    /**
+     * Сортирует ключи/значения, чтобы при Хэшировани был согласованный Хэш.
+     * (если перемешаются ключи то будет другой Хэш, что будет ошибкой,
+     * так как при повторных вычисениях блока нужно получить один и тот же Хэш)
+     *
+     * @param block словарь (Map) отсортированный по ключам
+     * @return json с отсортированными ключами
+     */
+    private static String getUnifiedBlock(SortedMap<String, String> block) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("{");
+        for (Map.Entry<String, String> entry : block.entrySet()) {
+            stringBuilder.append(entry.getKey());
+            stringBuilder.append(":");
+            stringBuilder.append(entry.getValue());
+            stringBuilder.append(",");
+        }
+        stringBuilder.append("}");
+
+        return stringBuilder.toString();
     }
 }
