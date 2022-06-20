df8e14f193861efc0b9e461bf83ddd8eee44b9f3
git-sergBond <git-serg.bond@yandex.ru>
2022-06-20 13:32:00 +0300

    add func new block

--- src/main/java/ss/bond/blockchain/domain/BlockChain.java
+++ src/main/java/ss/bond/blockchain/domain/BlockChain.java
@@ -1,19 +1,46 @@
 package ss.bond.blockchain.domain;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Map;
 
 public class BlockChain {
 
-    private ArrayList<Object> chain = new ArrayList<>();
+    private ArrayList<Map<String, String>> chain = new ArrayList<>();
     private ArrayList<Object> pendingTransactions = new ArrayList<>();
 
-    public void newBlock() {
-        //генерирует новый блок и добавляет его в цепь
+    public BlockChain() {
+        createGenesisBlock();
     }
 
-    public static void hash() {
-        //хэширует блок
+    /**
+     * Генерирует новый блок и добавляет его в цепь.
+     *
+     * @param previousHash предыдущий Хэш, Nullable
+     * @return новый блок
+     */
+    public Map<String, String> newBlock(String previousHash) {
+        Map<String, String> block = new java.util.HashMap<>(Map.of(
+                "index", String.valueOf(chain.size()),
+                "timestamp", LocalDateTime.now().toString(), //TODO посмотреть чтобы была timeZone и формат даты ISO
+                "previousHash", previousHash
+        ));
+
+        block.put("hash", BlockChain.hash(block));
+
+        chain.add(block);
+
+        return block;
+    }
+
+    /**
+     * Хэширует блок.
+     *
+     * @param block Блок от которого нужно получить Хэш
+     * @return Хэш в формате SHA256
+     */
+    public static String hash(Map<String, String> block) {
+        return null;
     }
 
     /**
@@ -36,5 +63,13 @@ public class BlockChain {
                 "recipient", recipient,
                 "amount", amount
         ));
+    }
+
+    /**
+     * Создание блока генезиса.
+     * Блок генезиса - жестко закодированный блок, с индексом 0 и не имеющий предшественников.
+     */
+    private void createGenesisBlock() {
+        newBlock(null);
     }
 }
