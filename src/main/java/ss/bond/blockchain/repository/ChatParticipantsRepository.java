09e14721b686fa59252b2c758223c66164a21ded
git-sergBond <git-serg.bond@yandex.ru>
2022-07-19 01:22:11 +0300

    imports

--- src/main/java/ss/bond/blockchain/repository/ChatParticipantsRepository.java
+++ src/main/java/ss/bond/blockchain/repository/ChatParticipantsRepository.java
@@ -2,9 +2,7 @@ package ss.bond.blockchain.repository;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
